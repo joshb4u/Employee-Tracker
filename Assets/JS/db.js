87f6021ccc61f2a7ad75f66c7228044fb85ae13b
joshb4u <59143348+joshb4u@users.noreply.github.com>
2020-05-05 15:57:49 -0400

    Added Index.js and installed packages

--- Assets/JS/db.js
+++ Assets/JS/db.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "!Babu601",
     database: "employeeTracker_db",
 });
 connection.connect(function (err) {
